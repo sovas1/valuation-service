b41b9fbf31e834d3e59b345dc32eb7affc678a33
sovas <mariuszsowa11@gmail.com>
2017-01-16 14:26:59 +0100

    implement CsvReader

--- src/main/java/org/sovas/util/io/CsvReader.java
+++ src/main/java/org/sovas/util/io/CsvReader.java
@@ -2,13 +2,34 @@ package org.sovas.util.io;
 
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CsvReader {
 
     public List<String> readLines(final String path) {
-        return null;
+        List<String> data = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(path))
+        )) {
+
+            data = reader.lines()
+                    .skip(1)
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            // TODO: 16.01.2017 logger
+            e.printStackTrace();
+        }
+
+        return data;
     }
 
 }

9c8b3a1092de539888a27c9ec1ae8e3b603a7ddb
sovas <mariuszsowa11@gmail.com>
2017-01-18 19:00:40 +0100

    test CsvParser - more tests

--- src/test/java/org/sovas/util/parser/CsvParserTest.java
+++ src/test/java/org/sovas/util/parser/CsvParserTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -31,6 +32,8 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 public class CsvParserTest {
 
+    // TODO: 18.01.2017 deeper tests
+
     @Autowired
     private CsvParser parser;
 
@@ -38,6 +41,19 @@ public class CsvParserTest {
     private CsvFileUtils fileUtils;
 
     private List<String> currencies = Stream.of("GBP,2.4", "EU,2.1", "PLN,1").collect(Collectors.toList());
+    private List<String> matchings = Stream.of("1,2", "2,2", "3,3").collect(Collectors.toList());
+    private List<String> products = Stream.of(
+            "1,1000,GBP,2,3",
+            "2,1050,EU,1,1",
+            "3,2000,PLN,1,1",
+            "4,1750,EU,2,2",
+            "5,1400,EU,4,3",
+            "6,7000,PLN,3,2",
+            "7,630,GBP,5,3",
+            "8,4000,EU,1,3",
+            "9,1400,GBP,3,1")
+            .collect(Collectors.toList());
+
 
     @Before
     public void setUp() {
@@ -45,16 +61,52 @@ public class CsvParserTest {
     }
 
     @Test
-    public void parseToCurrencies() {
+    public void testParseToCurrencies() {
+        // given
         given(fileUtils.readLines("currencies")).willReturn(currencies);
         List<String> linesMock = fileUtils.readLines("currencies");
 
+        // when
         List<Currency> currencies = parser.toCurrencies(linesMock);
 
+        // then
         assertNotNull(currencies);
         assertEquals(3, currencies.size());
         assertTrue(currencies.stream().anyMatch(e -> e.getCurrency().equals("GBP")));
-
     }
+
+    @Test
+    public void testParseToMatchings() {
+        // given
+        given(fileUtils.readLines("matchings")).willReturn(matchings);
+        List<String> linesMock = fileUtils.readLines("matchings");
+
+        // when
+        List<Matching> matchings = parser.toMatchings(linesMock);
+
+        // then
+        assertNotNull(matchings);
+        assertEquals(3, matchings.size());
+        // TODO: 18.01.2017 assert value
+        IntStream.range(0,3)
+                .forEach(i -> assertEquals(i, matchings.get(i).getId().intValue()));
+    }
+
+    @Test
+    public void testParseToProducts() {
+        // given
+        given(fileUtils.readLines("products")).willReturn(products);
+        List<String> linesMock = fileUtils.readLines("products");
+
+        // when
+        List<Product> products = parser.toProducts(linesMock);
+
+        // then
+        assertNotNull(products);
+        assertEquals(9, products.size());
+        // TODO: 18.01.2017 deeper tests
+    }
+
+
 
 }

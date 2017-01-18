cff1ada7dc7ee2a69d6ddbf6a1f17b2866bde3a1
sovas <mariuszsowa11@gmail.com>
2017-01-18 19:42:18 +0100

    test CsvParser - mock fix

--- src/test/java/org/sovas/util/parser/CsvParserTest.java
+++ src/test/java/org/sovas/util/parser/CsvParserTest.java
@@ -40,9 +40,9 @@ public class CsvParserTest {
     @MockBean
     private CsvFileUtils fileUtils;
 
-    private List<String> currencies = Stream.of("GBP,2.4", "EU,2.1", "PLN,1").collect(Collectors.toList());
-    private List<String> matchings = Stream.of("1,2", "2,2", "3,3").collect(Collectors.toList());
-    private List<String> products = Stream.of(
+    private List<String> mockCurrencies = Stream.of("GBP,2.4", "EU,2.1", "PLN,1").collect(Collectors.toList());
+    private List<String> mockMatchings = Stream.of("1,2", "2,2", "3,3").collect(Collectors.toList());
+    private List<String> mockProducts = Stream.of(
             "1,1000,GBP,2,3",
             "2,1050,EU,1,1",
             "3,2000,PLN,1,1",
@@ -63,47 +63,41 @@ public class CsvParserTest {
     @Test
     public void testParseToCurrencies() {
         // given
-        given(fileUtils.readLines("currencies")).willReturn(currencies);
-        List<String> linesMock = fileUtils.readLines("currencies");
 
         // when
-        List<Currency> currencies = parser.toCurrencies(linesMock);
+        List<Currency> currencies = parser.toCurrencies(mockCurrencies);
 
         // then
         assertNotNull(currencies);
-        assertEquals(this.currencies.size(), currencies.size());
+        assertEquals(this.mockCurrencies.size(), currencies.size());
         assertTrue(currencies.stream().anyMatch(e -> e.getCurrency().equals("GBP")));
     }
 
     @Test
     public void testParseToMatchings() {
         // given
-        given(fileUtils.readLines("matchings")).willReturn(matchings);
-        List<String> linesMock = fileUtils.readLines("matchings");
 
         // when
-        List<Matching> matchings = parser.toMatchings(linesMock);
+        List<Matching> matchings = parser.toMatchings(mockMatchings);
 
         // then
         assertNotNull(matchings);
-        assertEquals(this.matchings.size(), matchings.size());
+        assertEquals(this.mockMatchings.size(), matchings.size());
         // TODO: 18.01.2017 assert value
-        IntStream.range(0, this.matchings.size())
+        IntStream.range(0, this.mockMatchings.size())
                 .forEach(i -> assertEquals(i, matchings.get(i).getId().intValue()));
     }
 
     @Test
     public void testParseToProducts() {
         // given
-        given(fileUtils.readLines("products")).willReturn(products);
-        List<String> linesMock = fileUtils.readLines("products");
 
         // when
-        List<Product> products = parser.toProducts(linesMock);
+        List<Product> products = parser.toProducts(mockProducts);
 
         // then
         assertNotNull(products);
-        assertEquals(this.products.size(), products.size());
+        assertEquals(this.mockProducts.size(), products.size());
         // TODO: 18.01.2017 deeper tests
     }
 
